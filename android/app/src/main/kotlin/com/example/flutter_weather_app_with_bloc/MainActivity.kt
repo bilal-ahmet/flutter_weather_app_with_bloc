7e9524d7d08762368d38c12a206d16f64f70ed5b
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-02-28 13:28:55 +0300

    flutter_weather_app

---
+++ android/app/src/main/kotlin/com/example/flutter_weather_app_with_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_weather_app_with_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/weather_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
